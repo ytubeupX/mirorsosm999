06b548b907bdde01c90c1df75a76ed48ec38451e
ytubeupX <86553360+ytubeupX@users.noreply.github.com>
2022-06-14 10:13:27 +0600

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/tiktikimelbo7/potential-bassoon"
+    UPSTREAM_REPO = "https://github.com/ytubeupX/mirorsosm999"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
